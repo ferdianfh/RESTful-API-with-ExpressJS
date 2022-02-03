563252a7d41e11efa3f2283bc0a4ee9d3aa86579
ferdif <ferdifh82@gmail.com>
2022-02-03 13:03:10 +0700

    add middleware clearRedis in topup route

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const walletController = require("../controller/wallet");
 const { isAdmin, verifyAccess } = require("../middleware/auth");
+const { clearProfileInRedis } = require("../middleware/redis");
 
 const router = express.Router();
 
 router
   .get("/", verifyAccess, isAdmin, walletController.listWallets)
   .get("/details/:id", verifyAccess, isAdmin, walletController.detailsWallet)
-  .put("/topup", verifyAccess, walletController.topUp);
+  .put("/topup", verifyAccess, clearProfileInRedis, walletController.topUp);
 
 module.exports = router;
